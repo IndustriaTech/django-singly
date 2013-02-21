1867e9f64e48d8285fa690b712502b1172c38484
Venelin Stoykov <venelin@magicsolutions.bg>
2013-02-21 14:38:39 +0200

    Add simple template tag which will generate authentication url for the current user

---
+++ django_singly/templatetags/singly.py
@@ -0,0 +1,24 @@
+from django import template
+
+from django_singly.api import singly
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def singly_login_url(context, service):
+    """
+    Get URL to which user must be redirected to authenticate with Singly
+
+    If current user is already authenticated with singly
+    We must pass his authenticetion token to oauth URL.
+    Then Singly will know to connect two accounts from
+    different services into one.
+    """
+    user = context.get('user')
+    if user and user.is_authenticated():
+        profile = user.get_profile()
+        access_token = profile.singly_access_token
+    else:
+        access_token = None
+    return singly.get_authentication_url(service, access_token)
