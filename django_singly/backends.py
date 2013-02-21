484cadf19b7c9a6fd99479c19b84c1e8dc42a59a
Venelin Stoykov <venelin@magicsolutions.bg>
2013-02-21 19:17:23 +0200

    Improve singly backend
    Now it will get all profile data directly with authenticate request.
    Try to get user by email if in the profile data there is provided email.
    If fails to get a user create new one and try to get a profile (in most cases developers catch User create signal and immediatly create profile)
    If fail to get profile create new one.

--- django_singly/backends.py
+++ django_singly/backends.py
@@ -18,26 +18,42 @@ class SinglyBackend(ModelBackend):
     profile_model = get_model(*settings.AUTH_PROFILE_MODULE.split('.'))
 
     def authenticate(self, singly_code):
-        api = singly.authenticate(singly_code)
+        api = singly.authenticate(singly_code, profile='all', auth='true', full='true')
         if api:
             access_token = api.get_access_token()
             account = api.get_account()
+            profile_data = api.get_profile()  # This will works because profile is set to 'all' or 'last'
 
-            profile_data = api.profile.get() or {}
             try:
                 profile = self.profile_model.objects.select_related('user').get(account=account)
             except self.profile_model.DoesNotExist:
-                first_name, last_name = _get_names(profile_data.get('name', ''))
+                email = profile_data.get('email') or ''
+                user = None
 
-                user = User.objects.create(
-                    username=account,
-                    email=profile_data.get('email', ''),
-                    first_name=first_name,
-                    last_name=last_name,
-                )
+                if email:
+                    try:
+                        user = User.objects.get(email=email)
+                    except User.objects.DoesNotExist:
+                        pass
 
-                profile = self.profile_model(user=user, account=account)
+                if not user:
+                    first_name, last_name = _get_names(profile_data.get('name', ''))
+                    user = User.objects.create(
+                        username=account[:30],
+                        email=email,
+                        first_name=first_name,
+                        last_name=last_name,
+                    )
 
+                try:
+                    # If user is found by e-mail or there is a signal creating profile automaticaly
+                    # then we try to get this profile
+                    profile = user.get_profile()
+                except self.profile_model.DoesNotExist:
+                    # If it fails, then we create new profile
+                    profile = self.profile_model(user=user)
+
+            profile.account = account
             profile.singly_access_token = access_token
             profile.profile = profile_data
             profile.save()
